9b2fffa511b2f0576509738cd50a81b90c51a2e8
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-09-30 09:18:08 -0600

    primer push sin shaders

---
+++ Render.py
@@ -0,0 +1,129 @@
+from utilidades import * #Archivo de utilidades.
+
+class Render(object):
+
+    #Estas variables son globales y tienen valores por defecto y arbitrarios.
+    GRAY = color(0.501, 0.501, 0.501) #Color blanco hecho con las utilidades.
+    colorFondo = 0 #Asignando el color blanco al framebuffer.
+    #print("Color del fondo: ", colorFondo)
+    colorViewPort = GRAY #Asignando el color blanco al viewport. Esto es temporal-
+    width = 0 #Ancho de la pantalla. Esto es temporal.
+    height = 0 #Alto de la pantalla. Esto es temporal.
+    
+    widthV = 0 #Ancho del viewport. Esto es temporal.
+    heightV = 0 #Alto del viewport. Esto es temporal.
+
+    xV = 0 #Posición en x del viewport.
+    yV = 0 #Posición en y del viewport.
+
+    lista = None #Lista para generar el viewport.
+
+    colorP = 0 #Asignando el color blanco al punto. Esto es temporal.
+
+    framebuffer = [] #Framebuffer.
+
+    zBuffer = [] #Zbuffer.
+
+    zBufferE = [] #Copia del zbuffer, que servirá para escribir el archivo del zbuffer.
+
+    colorZ = 0 #Color del zbuffer.
+
+    tpath = None  #Path de las texturas. Aquí se inicializa en none porque aún no se sabe si hay texturas del personaje.
+
+    #Declarando una matriz para el modelo.
+    model_np = None #Quitar después.
+
+    model_s = None
+
+    #Declarando una matriz de vista.
+    view_np = None #Quitar después.
+
+    view = None
+
+    #Declarando la matriz de proyección.
+    Projection = None
+
+
+    #Método que escribe el archivo bmp.
+    def write(self): #Escribir un archivo, pero con el zbuffer.
+            
+            #Se abre el archivo con la forma de bw.
+            f = open("Proyecto_1.bmp", "bw")
+
+            #Se escribe el encabezado del archivo.
+
+            #Haciendo el pixel header.
+            f.write(char('B'))
+            f.write(char('M'))
+            #Escribiendo el tamaño del archivo en bytes.
+            f.write(dword(14 + 40 + self.width * self.height * 3))
+            f.write(dword(0)) #Cosa que no se utilizará en este caso.
+            f.write(dword(14 + 40)) #Offset a la información de la imagen. 14 bytes para el header, 40 para la información de la imagen. Aquí empieza la data.
+            #Lo anterior suma 14 bytes.
+
+            #Información del header.
+            f.write(dword(40)) #Este es el tamaño del header. Esto es de 4 bytes, por eso se utiliza el dword.
+            f.write(dword(self.width)) #Ancho de la imagen. Esto es de 4 bytes, por eso se utiliza el dword.
+            f.write(dword(self.height)) #Alto de la imagen. Esto es de 4 bytes, por eso se utiliza el dword.
+            f.write(word(1)) #Número de planos. Esto es de 2 bytes, por eso se utiliza el word.
+            f.write(word(24)) #24 bits por pixel. Esto es porque usa el true color y el RGB.
+            f.write(dword(0)) #Esto es la compresión. Esto es de 4 bytes, por eso se utiliza el dword.
+            f.write(dword(self.width * self.height * 3)) #Tamaño de la imagen sin el header.
+            #Pixels que no se usarán mucho.
+            f.write(dword(0))
+            f.write(dword(0))
+            f.write(dword(0))
+            f.write(dword(0))
+            #Lo anterior suma 40 bytes.
+
+            #print("Framebuffer", framebuffer)
+            #Pintando el archivo de color negro.
+            for y in range(self.height):
+                for x in range(self.width):
+                    f.write(self.framebuffer[y][x])
+
+            #print("Archivo escrito")
+
+            f.close() #Cerrando el archivo que se escribió.
+
+    #Método que escribe el archivo bmp.
+    def write2(self): #Escribir un archivo, pero con el zbuffer.
+            
+            #Se abre el archivo con la forma de bw.
+            f = open("SR4_2.bmp", "bw")
+
+            #Se escribe el encabezado del archivo.
+
+            #Haciendo el pixel header.
+            f.write(char('B'))
+            f.write(char('M'))
+            #Escribiendo el tamaño del archivo en bytes.
+            f.write(dword(14 + 40 + self.width * self.height * 3))
+            f.write(dword(0)) #Cosa que no se utilizará en este caso.
+            f.write(dword(14 + 40)) #Offset a la información de la imagen. 14 bytes para el header, 40 para la información de la imagen. Aquí empieza la data.
+            #Lo anterior suma 14 bytes.
+
+            #Información del header.
+            f.write(dword(40)) #Este es el tamaño del header. Esto es de 4 bytes, por eso se utiliza el dword.
+            f.write(dword(self.width)) #Ancho de la imagen. Esto es de 4 bytes, por eso se utiliza el dword.
+            f.write(dword(self.height)) #Alto de la imagen. Esto es de 4 bytes, por eso se utiliza el dword.
+            f.write(word(1)) #Número de planos. Esto es de 2 bytes, por eso se utiliza el word.
+            f.write(word(24)) #24 bits por pixel. Esto es porque usa el true color y el RGB.
+            f.write(dword(0)) #Esto es la compresión. Esto es de 4 bytes, por eso se utiliza el dword.
+            f.write(dword(self.width * self.height * 3)) #Tamaño de la imagen sin el header.
+            #Pixels que no se usarán mucho.
+            f.write(dword(0))
+            f.write(dword(0))
+            f.write(dword(0))
+            f.write(dword(0))
+            #Lo anterior suma 40 bytes.
+
+            #print("Framebuffer", framebuffer)
+            #Pintando el archivo de color negro.
+            for y in range(self.height):
+                for x in range(self.width):
+                    f.write(self.zBufferE[x][y])
+
+            #print("Archivo escrito")
+
+            f.close() #Cerrando el archivo que se escribió.
---
+++ main.py
@@ -0,0 +1,46 @@
+"""
+Referencias: 
+ 1. Acceder a una variable de una clase: https://www.codigopiton.com/variables-de-clase-y-de-instancia-en-python/#:~:text=Como%20puedes%20ver%2C%20para%20acceder,código%20definido%20en%20la%20clase.
+ 2. Acceder a un método de una clase: https://j2logo.com/python/tutorial/programacion-orientada-a-objetos/#:~:text=Para%20crear%20un%20objeto%20de,se%20llamara%20a%20una%20función).&text=El%20código%20anterior%20crea%20una,objeto%20a%20la%20variable%20obj%20.
+ 3. Inicializar una clase: https://www.tutorialesprogramacionya.com/pythonya/detalleconcepto.php?punto=44&codigo=44&inicio=30
+ 4. Clases en Python: https://tutorial.recursospython.com/clases/#:~:text=Para%20crear%20una%20clase%20vamos,mayúscula%2C%20y%20sin%20guiones%20bajos.
+"""
+#Archivo que tendrá el método main del programa
+
+from gl import * #Importando el archivo gl.py, para crear la imagen.
+from textures import * #Importando los métodos del archivo textures.py.
+
+def main():
+    glCreateWindow(2024, 2024) #Creando la ventana.
+    glClearColor(1, 1, 1) #Color del fondo.
+    glClear() #Limpiando el framebuffer con el color creado en glClearColor.
+    
+    glViewPort(600, 900, 300, 300) #Asignando el viewport.
+
+    col1 = (0.6, 0.1, 0.9) #Otro color.
+
+    #Probando el lookat.
+    # El medium shot se hizo con estas medidas: V3(25, 0, 10), V3(0, 1, 0), V3(0, 1, 0).
+    # El low angle se hizo con estas medidas: V3(10, -4.5, 0), V3(1, 0, 0), V3(0, 1, 0).
+    # El high agnle se hizo con estas medidas: V3(10, 12, 0), V3(1, 0, 0), V3(0, 1, 0) y las medidas 
+    # del viewport fueron glViewPort(900, 500, 300, 300).
+    # El dutch angle se hizo con estas medidas: scale = (0.75, 0.75, 1), translate = (1, 0.2, 0)
+    # rotacion = (0, 0, pi/2), glViewPort(700, 800, 300, 300) y lookAt(V3(0, 0, 10), V3(0, 1, 0), V3(0, 1, 0)). 
+    lookAt(V3(0, 0, 10), V3(0, 1, 0), V3(0, 1, 0))
+
+    scale = (1, 1, 1) #Escala para las cajas.
+    translate = (1, 0.2, 0) #Traslación para las cajas.
+    
+    rotacion = (0, 0, 0) #Rotación para las cajas.
+
+    print("Rotación: ", rotacion)
+    
+    #Esta llamada puede no estar acá.
+    loadModelMatrix(translate, scale, rotacion) #Se carga la matriz de transformación del modelo. Acá se recibe la traslación, la escala y la rotación.
+
+    #Esta función ahora recibe primero el path del obj, luego el path del bmp, el color.
+    modelo("./box.obj", "./box.bmp", col1) 
+
+    glFinish() #Escribiendo el framebuffer en la imagen y guardándola en un archivo.
+
+main()
